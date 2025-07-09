8db6c9a8013ea6263e6a79a4ecfa63cce253fccf
coba <ramonlg.daw@gmail.com>
2025-07-09 13:33:28 +0200

    Correcció ejercici 2 nivell 2

--- src/level2/exercise2/Main.java
+++ src/level2/exercise2/Main.java
@@ -1,27 +1,18 @@
 package level2.exercise2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import level2.exercise2.model.GenericMethodList;
 import level2.exercise2.model.Person;
 
 public class Main {
 
-
     public static void main(String[] args) {
+        Person person1 = new Person("John", "Lennon", 40);
+        Person person2 = new Person("Paul","McCartney", 83);
 
-        List<String> animalsList = new ArrayList<>();
-        animalsList.add("Horse");
-        animalsList.add("Bird");
-        animalsList.add("Lion");
+        System.out.println("Es passen 3 arguments al mètode printUndefinedNumberOfArguments:");
+        GenericMethodList.printUndefinedNumberOfArguments("Yesterday", 1960, person1);
 
-        List<Person> personsList = new ArrayList<>();
-        personsList.add(new Person("Juan", "Garcia", 22));
-        personsList.add(new Person("María", "Lopez", 32));
-        personsList.add(new Person("Paula", "Perez", 42));
-
-        GenericMethodList.genericList(animalsList);
-        GenericMethodList.genericList(personsList);
+        System.out.println("\nEs passen 5 arguments al mètode printUndefinedNumberOfArguments:");
+        GenericMethodList.printUndefinedNumberOfArguments("Yesterday" ,person2 , "Let it be", person1, 1960);
     }
 }
--- src/level2/exercise2/model/GenericMethodList.java
+++ src/level2/exercise2/model/GenericMethodList.java
@@ -1,13 +1,12 @@
 package level2.exercise2.model;
 
-import java.util.List;
-
 public class GenericMethodList {
 
-    public static <T> void genericList(List<T> listItems) {
-		for (T item : listItems) {
-			System.out.println(item);
-		}
-	}
-	
+    public static <T> void printUndefinedNumberOfArguments(T... arguments) {
+        for (T argument : arguments) {
+            System.out.println("- " + argument);
+        }
+    }
+
+
 }
